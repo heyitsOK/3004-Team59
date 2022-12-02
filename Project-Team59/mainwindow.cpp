e63ab3da89da0f0575a46be9a5dc0f4a80167229
heyitsOK <omark212378@gmail.com>
2022-12-02 16:01:42 -0500

    Added power button and wrote comments for implementation

--- Project-Team59/mainwindow.cpp
+++ Project-Team59/mainwindow.cpp
@@ -13,3 +13,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btnPower_released()
+{
+    //check if power is on already
+    //if it is not
+        //initialize everything
+        //display battery level
+        //set 2 minute timeout
+    //if power is on already
+        //ask or check whether the button was held for one second of just tapped
+            //if it was held we power off
+
+            //if it was tapped then we soft off
+}
